5c80114ebaa906dfe117843b8b288c95a92088aa
P1xt <P1xt@users.noreply.github.com>
2023-02-03 10:10:44 -0600

    feat: DRY project

--- 05-tdd-and-oop/projects/DRY-project/practice.js
+++ 05-tdd-and-oop/projects/DRY-project/practice.js
@@ -1,45 +1,21 @@
+const pick = (n1, n2, operation, callback) => {
+  return callback(operation(n1, n2));
+}
 function multiplyBiggerNumByTwo(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum * 2;
-  } else {
-    bigNum = num2;
-    return bigNum * 2;
-  }
+  return pick(num1, num2, Math.max, (input) => input * 2);
 }
 
 function divideBiggerNumByThree(num1, num2) {
-  let bigNum;
-  if (num1 > num2) {
-    bigNum = num1;
-    return bigNum / 3;
-  } else {
-    bigNum = num2;
-    return bigNum / 3;
-  }
+  return pick(num1, num2, Math.max, (input) => input  / 3);
 }
 
 function eatMostTacos(sum1, sum2) {
-  let bigNum;
-  if (sum1 > sum2) {
-    bigNum = sum1;
-    return `I ate ${bigNum} tacos.`;
-  } else {
-    bigNum = sum2;
-    return `I ate ${bigNum} tacos.`;
-  }
+  return pick(sum1, sum2, Math.max, (input) => `I ate ${input} tacos.`);
 }
 
 function adoptSmallerDog(weight1, weight2) {
-  let smallDog;
-  if (weight1 < weight2) {
-    smallDog = weight1;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  } else {
-    smallDog = weight2;
-    return `I adopted a dog that weighs ${smallDog} pounds.`;
-  }
+  return pick(weight1, weight2, Math.min,  
+    (input) => `I adopted a dog that weighs ${input} pounds.`);
 }
 
 
